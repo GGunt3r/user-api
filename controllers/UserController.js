9fd12ed28a0fb67d92448602219212c789584dfa
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-04-27 11:47:03 -0300

    Seção 24 - Aula 304. Deletando usuarios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -64,6 +64,19 @@ class UserController{
             res.send('Ocorreu um erro no servidor!');
         }
     }
+
+    async remove(req, res){
+        var id = req.params.id;
+
+        var result = await User.delete(id);
+        if(result.status){
+            res.status(200);
+            res.send('Usuário deletado com sucesso!');
+        }else{
+            res.status(406);
+            res.send(result.err);
+        }
+    }
 }
 
 module.exports = new UserController();
