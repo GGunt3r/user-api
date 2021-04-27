4a0c1159a3c3c938c132f80402b2ccce22b84f0d
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-04-27 11:25:30 -0300

    Nada modificado

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,7 +10,7 @@ class UserController{
         var id = req.params.id;
        var user = await User.findById(id);
         if(user == undefined){
-            res.status(404)
+            res.status(404);
             res.json({});
         }else{
             res.status(200);
@@ -24,21 +24,21 @@ class UserController{
 
         if(email == undefined){
             res.status(400);
-            res.json({err: "O e-mail é inválido ou não foi encontrado, tente novamente."})
+            res.json({err: "O e-mail é inválido ou não foi encontrado, tente novamente."});
             return;
         }
 
         if(nome == undefined){
             res.status(400);
-            res.json({err: "Nome indefinido ou não foi preenchido, tente novamente."})
+            res.json({err: "Nome indefinido ou não foi preenchido, tente novamente."});
             return;
         }
 
         var emailExists = await User.findEmail(email);
 
         if(emailExists){
-            res.status(406)
-            res.json({err: "O e-mail já foi cadastrado!"})
+            res.status(406);
+            res.json({err: "O e-mail já foi cadastrado!"});
         }
 
         
@@ -54,14 +54,14 @@ class UserController{
         if(result != undefined){
             if(result.status){
                 res.status(200);
-                res.send("Tudo ok!");
+                res.send('Tudo ok!');
             }else{
-                res.status(406)
-                res.send(result.err)
+                res.status(406);
+                res.send(result.err);
             }
         }else{
             res.status(406);
-            res.send("Ocorreu um erro no servidor!")
+            res.send('Ocorreu um erro no servidor!');
         }
     }
 }
