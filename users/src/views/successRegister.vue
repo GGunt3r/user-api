0060594c3919241d7f33a993ecd5beb0e29ed08f
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-05-01 12:06:52 -0300

    Correção na classe

--- users/src/views/successRegister.vue
+++ users/src/views/successRegister.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="notification is-success">
-        <h3 class=subtitle is-3>Cadastro realizado com sucecsso!</h3>
+        <h4 class="subtitle is-4">Cadastro realizado com sucecsso!</h4>
         O cadastro foi realizado com sucesso em nossa <strong>API</strong>. <br>
         Você já esta registrado em nosso banco de dados!
     </div>
