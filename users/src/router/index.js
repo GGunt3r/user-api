e4939922f18bca59ec66f376dca6dd0a3b676350
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-05-01 12:06:31 -0300

    Seção 25 - Aula 320. Login

--- users/src/router/index.js
+++ users/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
 import successRegister from '../views/successRegister'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
   path: '/complet',
   name: 'Complet',
   component: successRegister
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
---
+++ users/src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h4 class="subtitle is-4">Login</h4>
+        <hr>
+        <div class="columns is-centered">
+            <div class="column is-half">
+                <div v-if="error != undefined">
+                    <div class="notification is-danger">
+                        {{error}}
+                    </div>
+                </div>
+                <p>Email</p>
+                <input type="email" placeholder="email@exemple.com" class="input" v-model="email">
+                <p>Senha</p>
+                <input type="password" placeholder="******" class="input" v-model="password">
+                <hr>
+                <button class="button is-black" @click="login">Entrar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            password: '0',
+            email: '',
+            error: undefined
+        }
+    },
+    methods: {
+        login(){
+            console.log('login')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
