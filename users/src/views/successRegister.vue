1debba7d55ebe2cff9006e3f37eb84f977dd3713
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-05-01 11:58:20 -0300

    ✍ Página indicando que o cadastro foi feito.

---
+++ users/src/views/successRegister.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="notification is-success">
+        <h3 class=subtitle is-3>Cadastro realizado com sucecsso!</h3>
+        O cadastro foi realizado com sucesso em nossa <strong>API</strong>. <br>
+        Você já esta registrado em nosso banco de dados!
+    </div>
+</template>
