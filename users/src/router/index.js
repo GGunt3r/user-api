c0a1fcb30e93e74247d09264b8631e3a3b103fab
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-05-01 22:57:24 -0300

    Seção 25 - Aula 323 Autorização no back-end

--- users/src/router/index.js
+++ users/src/router/index.js
@@ -5,6 +5,29 @@ import Register from '../views/Register.vue'
 import successRegister from '../views/successRegister.vue'
 import Login from '../views/Login.vue'
 import Users from '../views/Users.vue'
+import axios from 'axios'
+
+function AdminAuth(to, from, next){
+  if(localStorage.getItem('token') != undefined){
+    
+    var req = {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    }
+    console.log(req);
+
+    axios.post('http://localhost:3000/validate', {}, req).then(res => {
+    console.log(res);
+    next();
+    }).catch(err => {
+      console.log(err.response);
+      next('/login');
+    });
+  }else{
+    next('/login');
+  }
+}
 
 Vue.use(VueRouter)
 
@@ -33,13 +56,7 @@ const routes = [
     path: '/admin/users',
     name: 'Users',
     component: Users,
-    beforeEnter: (to, from, next) => {
-      if(localStorage.getItem('token') != undefined){
-        next();
-      }else{
-        next('/login');
-      }
-    }
+    beforeEnter: AdminAuth
   },
   {
     path: '/about',
